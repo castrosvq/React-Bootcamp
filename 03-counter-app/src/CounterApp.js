cc2bafe21b04e2633d731dd2fa85ac9f90c3b6eb
Alba Castro <castrosvq@gmail.com>
2021-07-08 21:54:49 +0200

    onclick hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ProtoTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    // setCounter((c) => c + 1);
+  };
+
+  const handleReject = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
+
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleReject}>-1</button>
     </>
   );
 };
