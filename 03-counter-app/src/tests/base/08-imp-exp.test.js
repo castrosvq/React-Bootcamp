b1e45b566823c1e4b0ff7f44ac03342cc26d5392
Alba Castro <castrosvq@gmail.com>
2021-07-19 22:46:34 +0200

    tests

---
+++ 03-counter-app/src/tests/base/08-imp-exp.test.js
@@ -0,0 +1,33 @@
+import { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp";
+import heroes from "../../data/heroes";
+
+describe("Pruebas en funciones de heroes", () => {
+  test("debe de retornar un héroe por id", () => {
+    const id = 1;
+    const heroe = getHeroeById(id);
+
+    const heroeData = heroes.find((h) => h.id === id);
+    expect(heroe).toEqual(heroeData);
+  });
+
+  test("debe de retornar undefined si heroe no existe", () => {
+    const id = 10;
+    const heroe = getHeroeById(id);
+    expect(heroe).toBe(undefined);
+  });
+
+  test("debe de retornar un arreglo con los heroes de DC", () => {
+    const owner = "DC";
+    const heroe = getHeroesByOwner(owner);
+
+    const heroeData = heroes.filter((h) => h.owner === owner);
+    expect(heroe).toEqual(heroeData);
+  });
+
+  test("debe de retornar un arreglo con los heroes de Marvel", () => {
+    const owner = "Marvel";
+    const heroe = getHeroesByOwner(owner);
+
+    expect(heroe.length).toBe(2);
+  });
+});
