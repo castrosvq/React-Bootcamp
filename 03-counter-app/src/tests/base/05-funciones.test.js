36b0f16a1279217b13d921b278e115f0dfedbff6
Alba Castro <castrosvq@gmail.com>
2021-07-19 21:54:11 +0200

    more test

---
+++ 03-counter-app/src/tests/base/05-funciones.test.js
@@ -0,0 +1,24 @@
+import { getUser, getUsuarioActivo } from "../../base/05-funciones";
+
+describe("Pruebas de funciones", () => {
+  test("getUser debe retornar un objeto", () => {
+    const userTest = {
+      uid: "ABC123",
+      username: "El_Papi1502",
+    };
+    const user = getUser();
+
+    expect(user).toEqual(userTest);
+  });
+
+  test("getUsuarioActivo debe retornar un objeto", () => {
+    const nombre = "Juan";
+
+    const user = getUsuarioActivo(nombre);
+
+    expect(user).toEqual({
+      uid: "ABC567",
+      username: nombre,
+    });
+  });
+});
